e3164f3bad7e9552d26d8aad9ddc0da9ef371e2c
jovanr96 <alt.012.96@gmail.com>
2017-04-03 21:01:44 +0200

    Uvedene klase Kurs i Valuta, atributi i get i set metode

---
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -0,0 +1,38 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Valuta {
+	
+	private String naziv;
+	private String skraceniNaziv;
+	private GregorianCalendar datum;
+	private Kurs kurs;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	public Kurs getKurs() {
+		return kurs;
+	}
+	public void setKurs(Kurs kurs) {
+		this.kurs = kurs;
+	}
+	
+
+}
