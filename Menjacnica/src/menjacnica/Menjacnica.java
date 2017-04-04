39b2619e521136a83bf17184cc7d3ebf3d1adf91
jovanr96 <alt.012.96@gmail.com>
2017-04-04 02:08:29 +0200

    Definisane metode interfejsa Metode u klasi Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,27 +1,38 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import metode.Metode;
 
 public class Menjacnica implements Metode {
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	@Override
 	public void dodavanjeKursaValute(String naziv, String skraceniNaziv, GregorianCalendar datum, double kupovniKurs,
 			double prodajniKurs, double srednjiKurs) {
-		// TODO Auto-generated method stub
+
+		Kurs kurs = new Kurs(prodajniKurs, srednjiKurs, kupovniKurs);
+		Valuta valuta = new Valuta(naziv, skraceniNaziv, datum, kurs);
+		valute.add(valuta);
 
 	}
 
 	@Override
 	public void brisanjeKursaValuteZaOdredjeniDatum(String naziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++)
+			if (valute.get(i).getNaziv().equals(naziv) && valute.get(i).getDatum().equals(datum))
+				valute.remove(i);
 
 	}
 
 	@Override
 	public Kurs pronalazenjeKursaValuteZaOdredjeniDan(String naziv, String skraceniNaziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++)
+			if (valute.get(i).getNaziv().equals(naziv) && 
+					valute.get(i).getSkraceniNaziv().equals(skraceniNaziv)
+					&& valute.get(i).getDatum().equals(datum))
+				return valute.get(i).getKurs();
 		return null;
 	}
 
