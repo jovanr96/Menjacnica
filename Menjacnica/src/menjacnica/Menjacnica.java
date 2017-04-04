847b8fb321d6ddc7757aa8f650003b4f33ce161e
jovanr96 <alt.012.96@gmail.com>
2017-04-04 02:00:08 +0200

    Uvedena klasa Menjacnica koja implementira interfejs Metode

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,28 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import metode.Metode;
+
+public class Menjacnica implements Metode {
+
+	@Override
+	public void dodavanjeKursaValute(String naziv, String skraceniNaziv, GregorianCalendar datum, double kupovniKurs,
+			double prodajniKurs, double srednjiKurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void brisanjeKursaValuteZaOdredjeniDatum(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs pronalazenjeKursaValuteZaOdredjeniDan(String naziv, String skraceniNaziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
